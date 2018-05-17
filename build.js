ae3140c33bad5dc7be48572eb2800ccf81e0166b
Josh <og.ginger@gmail.com>
2018-05-17 09:13:15 +0000

    rjs_optimize: r.js is the requirejs optimizer- it is used for concatenating dependecies and minifying the files; build.js is the configuration file used by r.js.

---
+++ build.js
@@ -0,0 +1,11 @@
+//build.js: Configuration file for optimizing the ifSetup function. 
+/*
+*/ 
+({
+    paths: {
+	"log": "lib/log.min"
+    },
+    exclude: [ "log" ],
+    name: "project",
+    out: "project.min.js"
+})
